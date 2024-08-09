0c1190c86b8e4fd775c0291065146be9cdf1c3df
Vincent Lamanna <vincent@crewdle.com>
2024-08-09 15:38:44 -0400

    Fix multi-upload

--- src/models/OfficeParserConnector.ts
+++ src/models/OfficeParserConnector.ts
@@ -2,14 +2,48 @@ import officeParser from 'officeparser';
 
 import { IFile, IDocumentParserConnector } from '@crewdle/web-sdk-types';
 
+interface IParserQueue {
+  file: IFile;
+  resolve: (data: string) => void;
+  reject: (error: Error) => void;
+}
+
 export class OfficeParserConnector implements IDocumentParserConnector{
+  private static queue: IParserQueue[] = [];
+  private static isProcessing = false;
+
   public async parse(file: IFile): Promise<string> {
     try {
-      const buffer = Buffer.from(await file.arrayBuffer());
-      const data = await officeParser.parseOfficeAsync(buffer);
-      return data;
+      return new Promise<string>((resolve, reject) => {
+        OfficeParserConnector.queue.push({ file, resolve, reject });
+        OfficeParserConnector.processQueue();
+      });
     } catch (e) {
       throw new Error(`Failed to parse the file: ${e}`);
     }
+  }
+
+  private static async processQueue(): Promise<void> {
+    if (OfficeParserConnector.isProcessing) {
+      return;
+    }
+
+    OfficeParserConnector.isProcessing = true;
+
+    while (OfficeParserConnector.queue.length > 0) {
+      const { file, resolve, reject } = OfficeParserConnector.queue.shift() as IParserQueue;
+      try {
+        const buffer = Buffer.from(await file.arrayBuffer());
+        const data = await officeParser.parseOfficeAsync(buffer);
+        resolve(data);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      } catch (e) {
+        if (e instanceof Error) {
+          reject(e);
+        }
+      }
+    }
+
+    OfficeParserConnector.isProcessing = false;
   }
 }
