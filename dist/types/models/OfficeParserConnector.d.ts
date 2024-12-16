1315548f49cc5d17d4f9630f588523cf0c18add9
Vincent Lamanna <vincent@crewdle.com>
2024-12-15 22:11:25 -0500

    Allow to pass File.

--- dist/types/models/OfficeParserConnector.d.ts
+++ dist/types/models/OfficeParserConnector.d.ts
@@ -2,8 +2,8 @@ import { IFile, IDocumentParserConnector } from '@crewdle/web-sdk-types';
 export declare class OfficeParserConnector implements IDocumentParserConnector {
     private static queue;
     private static isProcessing;
-    parse(file: IFile): Promise<string>;
+    parse(file: IFile | File): Promise<string>;
     getSupportedFileTypes(): string[];
-    supports(file: IFile): boolean;
+    supports(file: IFile | File): boolean;
     private static processQueue;
 }
